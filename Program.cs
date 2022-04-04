997d0afb3fb58d708bf566fcf2d028a9b41d3b0a
LucasHotaru <lucashotaru01@gmail.com>
2022-04-04 16:56:30 -0300

    UpdateLogin

--- Program.cs
+++ Program.cs
@@ -1,41 +1,20 @@
+using System.Text;
 using AngularCBFBackEND;
-using AngularCBFBackEND.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using AngularCBFBackEND.Config;
 using System.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 //conexao banco
 
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Curso")));
-
-builder.Services.AddDbContext<AppIdentityDbContext> (options =>
-    options.UseSqlServer(Configuration.GetConnectionString("Default")));
-
 // Add services to the container.
 
-
-
-builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("Devlopment",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
-builder.Services.AddMvc();
-builder.Services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
 
 
 builder.Services.AddControllers();
@@ -52,15 +31,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseIdentityServer();
 
-app.UseStaticFiles();
-
-app.UseMvcWithDefaultRoute();
-
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseRouting();
 
 app.MapControllers();
 
