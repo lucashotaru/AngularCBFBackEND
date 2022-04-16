f9e263291213c5fa74cf09e197c445645e510613
Lucas <lucashotaru01@gmail.com>
2022-04-16 01:08:10 -0300

    WebScraping Update

--- conteudo/PainelAdmin/Factory/PainelAdminFactory.cs
+++ conteudo/PainelAdmin/Factory/PainelAdminFactory.cs
@@ -1,4 +1,6 @@
+using AngularCBFBackEND.conteudo.PainelAdmin.Models;
 using AngularCBFBackEND.conteudo.PainelAdmin.Repositories;
+using HtmlAgilityPack;
 
 namespace AngularCBFBackEND.conteudo.PainelAdmin.Factory
 {
@@ -6,9 +8,93 @@ namespace AngularCBFBackEND.conteudo.PainelAdmin.Factory
     {
         public static async Task<Byte[]> GetExcelCBF(string tipo, string serie)
         {
-            var GetListaJogos = await WebScrapingJogosRepository.GetCBFInfo(tipo, serie);
+            List<JogosModel> Lista = new List<JogosModel>();
 
-            var GetArquivoExcel = await WebScrapingJogosRepository.ConverterListarCBFEmExcel(GetListaJogos);
+            int[] dataConvertida = new int[2];
+            dataConvertida = WebScrapingJogosRepository.verificaData(tipo, serie);
+
+            bool continuar = true;
+
+            for (int ano = dataConvertida[0]; ano < dataConvertida[1]; ano++)
+            {       
+                int RodadaCount = 1, urlAtual = 0;
+
+                string url = $"https://www.cbf.com.br/futebol-brasileiro/competicoes/{tipo}-{serie}/{ano}";
+
+                var urlVerificada = await WebScrapingJogosRepository.ValidaFaseDaSerie(url);
+                
+                do
+                {
+                    HtmlWeb web = new HtmlWeb();
+                    HtmlDocument doc = web.Load(urlVerificada[urlAtual]);
+
+                    int idVar = 0;;
+
+                    if(urlVerificada.Count() > 0)
+                    {
+                        doc = web.Load(urlVerificada[urlAtual]);
+                        continuar = true;
+                    }
+                    
+                    if(urlAtual == urlVerificada.Count() - 1)
+                        continuar = false;
+
+                    urlAtual++;
+
+                    try
+                    {
+                        var cbfNomeTimeCasaNode = doc.DocumentNode.SelectNodes("//div[@class='time pull-left']/img");
+                        var cbfNomeTimeVisitanteNode = doc.DocumentNode.SelectNodes("//div[@class='time pull-right']/img");
+                        var DataHoraNode = doc.DocumentNode.SelectNodes("//span[@class='partida-desc text-1 color-lightgray p-b-15 block uppercase text-center']");
+                        var PlacarNode = doc.DocumentNode.SelectNodes("//div[@class='clearfix']/a/strong[@class='partida-horario center-block']");
+                        var RodadaNove = doc.DocumentNode.SelectNodes("//h3[@class='text-center']");
+
+                        string[] cbfNomeTimeCasa = new string[cbfNomeTimeCasaNode.Count];
+                        string[] cbfNomeTimeVisitante = new string[cbfNomeTimeVisitanteNode.Count];
+                        string[] Placar = new string[PlacarNode.Count];
+                        int[] Rodada = new int[cbfNomeTimeCasaNode.Count];
+                        string[] Data = new string[DataHoraNode.Count];
+
+                        foreach (var item in cbfNomeTimeCasaNode)
+                        {
+                            cbfNomeTimeCasa[idVar] = cbfNomeTimeCasaNode[idVar].GetAttributeValue("title", string.Empty).ToString();
+                            cbfNomeTimeVisitante[idVar] = cbfNomeTimeVisitanteNode[idVar].GetAttributeValue("title", string.Empty).ToString();
+                            Placar[idVar] = PlacarNode[idVar].InnerText.ToString().Trim();
+                            Data[idVar] = DataHoraNode[idVar].InnerText.ToString().Trim().Remove(0, 5);
+
+                            int[] placarConvertido = new int[2];
+
+                            placarConvertido = WebScrapingJogosRepository.FormatarPlacar(Placar[idVar]);
+
+                            DateTime DataConvertida = WebScrapingJogosRepository.FormataData(Data[idVar]);
+
+                            Rodada[idVar] = RodadaCount;
+
+                            var result = new JogosModel(cbfNomeTimeCasa[idVar],
+                                                        placarConvertido[0],
+                                                        cbfNomeTimeVisitante[idVar],
+                                                        placarConvertido[1],
+                                                        Rodada[idVar],
+                                                        DataConvertida,
+                                                        serie,
+                                                        ano);
+                                                        
+                            Lista.Add(result);
+                            
+                            idVar++;
+                        }
+                        Console.WriteLine("Leitura :" + ano);
+                    }
+                    catch (System.Exception)
+                    {
+                        throw;
+                    }
+
+
+                } while (continuar == true);
+            }
+        
+            var GetArquivoExcel = await WebScrapingJogosRepository.ConverterListarCBFEmExcel(Lista);
 
             return GetArquivoExcel;
         } 
--- conteudo/PainelAdmin/Repositories/WebScrapingJogosRepository.cs
+++ conteudo/PainelAdmin/Repositories/WebScrapingJogosRepository.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Text.RegularExpressions;
 using AngularCBFBackEND.conteudo.PainelAdmin.Models;
 using ClosedXML.Excel;
 using HtmlAgilityPack;
@@ -7,143 +8,119 @@ namespace AngularCBFBackEND.conteudo.PainelAdmin.Repositories
 {
     public class WebScrapingJogosRepository
     {
-        public static async Task<List<JogosModel>> GetCBFInfo(string tipo, string serie)
-        {
-            List<JogosModel> Lista = new List<JogosModel>();
-
-            DateTime dataAgora = DateTime.Now;
-            int dataConvertida = dataAgora.Year;
-
-            for (int y = 2012; y < dataConvertida; y++)
-            {
-                HtmlWeb web = new HtmlWeb();
-                HtmlDocument doc = web.Load($"https://www.cbf.com.br/futebol-brasileiro/competicoes/{tipo}-{serie}/{y}");
-
-                try
-                {
-                    var cbfNomeTimeCasaNode = doc.DocumentNode.SelectNodes("//div[@class='time pull-left']/img");
-                    var cbfNomeTimeVisitanteNode = doc.DocumentNode.SelectNodes("//div[@class='time pull-right']/img");
-                    var DataHoraNode = doc.DocumentNode.SelectNodes("//span[@class='partida-desc text-1 color-lightgray p-b-15 block uppercase text-center']");
-                    var PlacarNode = doc.DocumentNode.SelectNodes("//div[@class='clearfix']/a/strong[@class='partida-horario center-block']");
-
-                    int i = 0, r = 10, rr = 1;
-                    string[] cbfNomeTimeCasa = new string[cbfNomeTimeCasaNode.Count];
-                    string[] cbfNomeTimeVisitante = new string[cbfNomeTimeVisitanteNode.Count];
-                    string[] PlacarCasa = new string[PlacarNode.Count];
-                    string[] PlacarVisitante = new string[PlacarNode.Count];
-                    int[] Rodada = new int[cbfNomeTimeCasaNode.Count];
-                    string[] Data = new string[DataHoraNode.Count];
-                    DateTime[] Date = new DateTime[DataHoraNode.Count];
-                    Rodada[0] = 0;
-
-
-                    foreach (var item in cbfNomeTimeCasaNode)
-                    {
-                        cbfNomeTimeCasa[i] = cbfNomeTimeCasaNode[i].GetAttributeValue("alt", string.Empty).ToString();
-
-                        cbfNomeTimeVisitante[i] = cbfNomeTimeVisitanteNode[i].GetAttributeValue("alt", string.Empty).ToString();
-
-                        PlacarCasa[i] = PlacarNode[i].InnerText.ToString().Trim();
-                        PlacarVisitante[i] = PlacarNode[i].InnerText.ToString().Trim();
-
-                        PlacarCasa[i] = PlacarCasa[i].Remove(PlacarCasa[i].Length - 4);
-                        if (PlacarCasa[i].Length > 1)
-                        {
-                            PlacarCasa[i] = PlacarCasa[i].Remove(0, 3);
-                        }
-                        PlacarVisitante[i] = PlacarVisitante[i].Remove(0, 4);
-                        if (PlacarVisitante[i].Length > 1)
-                        {
-                            PlacarVisitante[i] = PlacarVisitante[i].Remove(0, 2);
-                        }
-
-                        Data[i] = DataHoraNode[i].InnerText.ToString().Trim();
-                        Data[i] = Data[i].Remove(0, 5);
-                        while (Data[i].Length > 16)
-                        {
-                            Data[i] = Data[i].Remove(Data[i].Length - 1);
-                        }
-
-                        if (i + 1 > r)
-                        {
-                            rr++;
-                            r += 10;
-                        }
-                        Rodada[i] = rr;
-
-                        DateTime data = DateTime.ParseExact(Data[i], "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-                        
-                            var result = new JogosModel(cbfNomeTimeCasa[i],
-                                                        Convert.ToInt32(PlacarCasa[i]),
-                                                        cbfNomeTimeVisitante[i],
-                                                        Convert.ToInt32(PlacarVisitante[i]),
-                                                        Rodada[i],
-                                                        data,
-                                                        serie,
-                                                        y);
-                            try
-                            {
-
-                                Lista.Add(result);
-                            }
-                            catch (Exception ex)
-                            {
-
-                                Console.WriteLine(ex);
-                            }
-
-                        i++;
-                    }
-                    Console.WriteLine("Leitura :" + y);
-                }
-                catch (System.Exception)
-                {
-
-                    throw;
-                }
-            }
-            return Lista;
-        }
-
         public static async Task<Byte[]> ConverterListarCBFEmExcel(List<JogosModel> lista)
         {
             
-            return await Task.Run(async ()=>{
+            return await Task.Run(() =>
+            {
 
-                    var workbook = new XLWorkbook();
-                    var worksheet = workbook.Worksheets.Add("TabelaJogos");
-                    var linha = 1;
-                    
-                    worksheet.Cell(linha, 1).Value = "NomeTimeCasa";
-                    worksheet.Cell(linha, 2).Value = "PlacarTimeCasa";
-                    worksheet.Cell(linha, 3).Value = "NomeTimeVisitante";
-                    worksheet.Cell(linha, 4).Value = "PlacarTimeVisitante";
-                    worksheet.Cell(linha, 5).Value = "Rodada";
-                    worksheet.Cell(linha, 6).Value = "DataHoraJogo";
-                    worksheet.Cell(linha, 7).Value = "Serie";
-                    worksheet.Cell(linha, 7).Value = "Serie";
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("TabelaJogos");
+                var linha = 1;
 
-                    foreach (var tabela in lista)
-                        {
-                            linha++;
-                            worksheet.Cell(linha, 1).Value = tabela.NomeTimeCasa;
-                            worksheet.Cell(linha, 2).Value = tabela.PlacarTimeCasa;
-                            worksheet.Cell(linha, 3).Value = tabela.NomeTimeVisitante;
-                            worksheet.Cell(linha, 4).Value = tabela.PlacarTimeVisitante;
-                            worksheet.Cell(linha, 5).Value = tabela.Rodada;
-                            worksheet.Cell(linha, 6).Value = tabela.DataHoraJogo;
-                            worksheet.Cell(linha, 7).Value = tabela.Serie;
-                            worksheet.Cell(linha, 8).Value = tabela.AnoTabela;
-                        }
-                    
-                    var stream = new MemoryStream();
-                    workbook.SaveAs(stream);
-                    var content = stream.ToArray();
+                worksheet.Cell(linha, 1).Value = "NomeTimeCasa";
+                worksheet.Cell(linha, 2).Value = "PlacarTimeCasa";
+                worksheet.Cell(linha, 3).Value = "NomeTimeVisitante";
+                worksheet.Cell(linha, 4).Value = "PlacarTimeVisitante";
+                worksheet.Cell(linha, 5).Value = "Rodada";
+                worksheet.Cell(linha, 6).Value = "DataHoraJogo";
+                worksheet.Cell(linha, 7).Value = "Serie";
+                worksheet.Cell(linha, 8).Value = "AnoTabela";
 
-                    return content;
-                    }
-                );
+                foreach (var tabela in lista)
+                {
+                    linha++;
+                    worksheet.Cell(linha, 1).Value = tabela.NomeTimeCasa;
+                    worksheet.Cell(linha, 2).Value = tabela.PlacarTimeCasa;
+                    worksheet.Cell(linha, 3).Value = tabela.NomeTimeVisitante;
+                    worksheet.Cell(linha, 4).Value = tabela.PlacarTimeVisitante;
+                    worksheet.Cell(linha, 5).Value = tabela.Rodada;
+                    worksheet.Cell(linha, 6).Value = tabela.DataHoraJogo;
+                    worksheet.Cell(linha, 7).Value = tabela.Serie;
+                    worksheet.Cell(linha, 8).Value = tabela.AnoTabela;
+                }
+
+                var stream = new MemoryStream();
+                workbook.SaveAs(stream);
+                var content = stream.ToArray();
+
+                return content;
+            });
+        }
+
+
+        public static int[] FormatarPlacar(string Placar)
+        {
+            int[] placar = new int[2];
+
+            var a = Regex.Replace(Placar, @"[:><'!@#$%*-+?;.,ç~{}x()\0 ]", "");
+
+            placar[0] = Convert.ToInt32(a.Remove(a.Length - 1));
+            placar[1] = Convert.ToInt32(a.Remove(0, 1));
+
+            return placar;
+        }
+
+         public static DateTime FormataData(string data)
+        {
+            var Data = Regex.Replace(data, @"[><'!@#$%*-+?;.,ç~{}x()\0rnJogo]", "");
+
+            while(Data.Length > 16)
+            {
+                Data = Data.Remove(Data.Length - 1);
+            }
+
+            DateTime dataConvertida = DateTime.ParseExact(Data, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            return dataConvertida;
+        }
+
+        public static async Task<string[]> ValidaFaseDaSerie(string urlfac)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(urlfac);
+
+            int valida = 0;
+
+            var validaUrlNode = doc.DocumentNode.SelectNodes("//select[@class='form-control big auto-select']/option");
+
+            if(validaUrlNode == null)
+            {
+                string[] url = new string[1];
+                url[0] = urlfac;
+
+                return url;
+            }
+            else
+            {
+                string[] url = new string[validaUrlNode.Count];
+
+                foreach (var item in validaUrlNode)
+                {
+                    url[valida] = validaUrlNode[valida].GetAttributeValue("value", string.Empty).ToString();
+                    valida++;
+                }
+
+                return url;
+            }
+
+        }
+
+        public static int[] verificaData(string tipo, string serie)
+        {
+            int[] dataConvertida = new int[2];
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load($"https://www.cbf.com.br/futebol-brasileiro/competicoes/{tipo}-{serie}");
             
+            var dataConvertidaNode = doc.DocumentNode.SelectNodes("//select[@class='form-control auto-select']/option");
+
+            int cont = dataConvertidaNode.Count;
+
+            dataConvertida[1] = Convert.ToInt32(dataConvertidaNode[0].InnerHtml);
+            dataConvertida[0] = Convert.ToInt32(dataConvertidaNode[cont - 1].InnerHtml);
+
+            return dataConvertida;
         }
     }
 }
